716eb1c002a8c2d1804c66bc0d47c193ccf1ca33
Aliu Salaudeen <ayokunumi99@gmail.com>
2024-03-01 10:34:00 +0100

    working on layout fix

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -25,13 +25,13 @@ const Hero = () => {
 		<section className='relative w-full bg-black'>
 			<div
 				id='hero'
-				className='container h-screen md:h-[calc(100vh-64px)] -mt-[60px] md:mt-0 flex items-center md:items-start justify-center pt-0 md:pb-[200px] md:pt-[180px]'
+				className='container h-screen md:h-[calc(100vh-64px)] -mt-[70px] md:mt-0 flex items-center md:items-start justify-center pt-0 md:pb-[200px] md:pt-[180px]'
 			>
 				<div className='flex flex-col items-center'>
 					<div className='overflow-hidden'>
 						<div
 							id='logo'
-							className='w-[200px] md:w-[321px] h-[109px] md:h-[136px]'
+							className='w-[200px] md:w-[321px] h-fit md:h-[136px]'
 						>
 							<Image
 								src={'/svgs/debuka.svg'}
@@ -42,7 +42,6 @@ const Hero = () => {
 							/>
 						</div>
 					</div>
-
 					<span className='text-primary font-extralight mt-2 md:mt-6 text-sm md:text-xl leading-[26px] pl-[0.5em] tracking-[0.5em]'>
 						BEYOND FOOD
 					</span>
